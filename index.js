cbecf67791a89a73e37ac9c5f2fb9ac2afe4bbc6
Yaroslava Nahorna <yasya74800gmail.com>
2024-08-20 01:01:34 +0300

    Created project basic files

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', function (req, res) {
+    res.render('index')
+})
+app.listen(3000)
