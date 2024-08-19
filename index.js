d7d2a2e372a6e9941c5e1bf65a216b35f5776836
Yaroslava Nahorna <yasya74800gmail.com>
2024-08-20 01:47:13 +0300

    Refactored code dublicate with Promice for data fetching

--- index.js
+++ index.js
@@ -10,39 +10,40 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 const key = '1p63a1bc8f17b0cb7c9a123c2a4dwac1';
-let city = 'Tartu'
-
-app.get('/', function (req, res) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-.then(response => {
-  return response.json()
-})
-.then(data => {
-    let description = data.weather[0].description;
-let city = data.name;
-let temp = Math.round(parseFloat(data.main.temp)-274.15)
-res.render('index', {
-    description: description,
-    city: city,
-    temp: temp
-})
-})
-})
-app.post('/', function(req, res) {
-    let city = req.body.cityname;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-    .then(response => {
-      return response.json()
+const getWeatherDataPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(responce => {
+                return responce.json()
+            })
+            .then(data => {
+                let description = data.weather[0].description
+                let city = data.name
+                let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+                let result = {
+                    description: description,
+                    city: city,
+                    temp: temp
+                }
+                resolve(result)
+            })
+            .catch(error => {
+                reject(error)
+            })
+        })
+    }
+    app.all('/', function (req, res){
+        let city
+        if(req.method == 'GET'){
+            city = 'Tartu'
+        }
+        if(req.method == 'POST'){
+            city = req.body.cityname
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+        getWeatherDataPromise(url).then(data => {
+            res.render('index', data)
+        })
     })
-    .then((data) => {
-      let description = data.weather[0].description;
-      let city = data.name;
-      let temp = Math.round(parseFloat(data.main.temp) - 273.15)
-      res.render('index', {
-        description: description,
-        city: city,
-        temp: temp
-      });
-    })
-  })
+    
 app.listen(3000)
