526c18dbcb469d8ed78f7c7169f77115129542d2
Yaroslava Nahorna <yasya74800gmail.com>
2024-08-20 01:39:43 +0300

    Created form request responce with API data

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -26,4 +28,21 @@ res.render('index', {
 })
 })
 })
+app.post('/', function(req, res) {
+    let city = req.body.cityname;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then(response => {
+      return response.json()
+    })
+    .then((data) => {
+      let description = data.weather[0].description;
+      let city = data.name;
+      let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+      res.render('index', {
+        description: description,
+        city: city,
+        temp: temp
+      });
+    })
+  })
 app.listen(3000)
